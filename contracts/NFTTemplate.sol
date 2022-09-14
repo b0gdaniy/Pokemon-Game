8cc0ac7b0e3b55baa55288d7d994765c05f7b816
Bohdan Pukhno <pukhnobg@gmail.com>
2022-09-14 19:31:09 +0300

    update: Removed URIStorage

---
+++ contracts/NFTTemplate.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTTemplate is ERC721, Ownable {
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://...";
+    }
+}
