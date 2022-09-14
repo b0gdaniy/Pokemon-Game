468d8e876abf9babe62beede4b2757b54e8e6a21
Bohdan Pukhno <pukhnobg@gmail.com>
2022-09-14 23:59:06 +0300

    update: Added burn function, replaced URI function

--- contracts/NFTTemplate.sol
+++ contracts/NFTTemplate.sol
@@ -15,8 +15,20 @@ contract NFTTemplate is ERC721, Ownable {
         _safeMint(to, tokenId);
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://...";
+    /**
+     * @dev Burns `tokenId`. See {ERC721-_burn}.
+     *
+     * REQUIREMENTS:
+     *
+     * - The caller must own `tokenId` or be an approved operator.
+     */
+    function burn(uint256 tokenId) internal {
+        //solhint-disable-next-line max-line-length
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not token owner nor approved"
+        );
+        _burn(tokenId);
     }
 
     function random() internal returns (uint256) {
@@ -25,5 +37,9 @@ contract NFTTemplate is ERC721, Ownable {
             uint256(
                 keccak256(abi.encode(block.timestamp, msg.sender, nonce++))
             ) % 100;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://...";
     }
 }
