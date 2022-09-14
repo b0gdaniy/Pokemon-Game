af349653219e23c67f5c02b3d15850efdb727149
Bohdan Pukhno <pukhnobg@gmail.com>
2022-09-14 18:41:12 +0300

    update: Removed burnable

--- contracts/NFTTemplate.sol
+++ contracts/NFTTemplate.sol
@@ -3,10 +3,9 @@ pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTTemplate is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract NFTTemplate is ERC721, ERC721URIStorage, Ownable {
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
     {}
