514090852c5b29695c83695c5c421301c4aa6a31
Bohdan Pukhno <pukhnobg@gmail.com>
2022-09-14 18:52:23 +0300

    update: Added receive() for mint PLVL to sender

--- contracts/PokemonLevelToken.sol
+++ contracts/PokemonLevelToken.sol
@@ -6,10 +6,22 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PokemonLevelToken is ERC20, Ownable {
-    constructor() ERC20("PokemonLevel", "PLVL") {
-        _mint(msg.sender, 1000 * 10**decimals());
+    constructor() ERC20("PokemonLevel", "PLVL") {}
+
+    /**
+     * @dev Minting `msg.value` tokens to `msg.sender`
+     * @notice Level up to Pokemon
+     */
+    receive() external payable {
+        _mint(msg.sender, msg.value * 10**decimals());
     }
 
+    /**
+     * @dev Minting `amount` of tokens to `to`
+     *
+     * REQUIREMENTS:
+     * - it can be called only by `owner` of this contract
+     */
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
