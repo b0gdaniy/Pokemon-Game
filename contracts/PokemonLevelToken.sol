d3841463b2b72ec8e0bcb5fb386f9a8b21cf45f2
Bohdan Pukhno <pukhnobg@gmail.com>
2022-09-14 23:39:50 +0300

    update: Comments

--- contracts/PokemonLevelToken.sol
+++ contracts/PokemonLevelToken.sol
@@ -9,18 +9,18 @@ contract PokemonLevelToken is ERC20, Ownable {
     constructor() ERC20("PokemonLevel", "PLVL") {}
 
     /**
-     * @dev Minting `msg.value` tokens to `msg.sender`
-     * @notice Level up to Pokemon
+     * @dev Minting `msg.value` tokens to `msg.sender`.
+     * @notice Level up to Pokemon.
      */
     receive() external payable {
         _mint(msg.sender, msg.value * 10**decimals());
     }
 
     /**
-     * @dev Minting `amount` of tokens to `to`
+     * @dev Minting `amount` of tokens to `to`.
      *
      * REQUIREMENTS:
-     * - it can be called only by `owner` of this contract
+     * - The caller must be the owner of this contract.
      */
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
