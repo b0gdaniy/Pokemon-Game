fef6d9e71032324e27a55b31b14b5153a3d4915b
Bohdan Pukhno <pukhnobg@gmail.com>
2022-09-19 00:23:01 +0300

    add: Added gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 require('dotenv').config();
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -17,6 +18,10 @@ const config: HardhatUserConfig = {
       url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`]
     }
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'ETH'
   }
 };
 
