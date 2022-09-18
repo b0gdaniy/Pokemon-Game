2724252953765eb2de436cedaded3b56ed32bade
Bohdan Pukhno <pukhnobg@gmail.com>
2022-09-19 01:25:44 +0300

    add: For change PokemonToken bytes size

---
+++ contracts/PokemonNames.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract PokemonNames {
+    function firstStageNames(uint256 _index)
+        external
+        pure
+        returns (string memory)
+    {
+        string[5] memory _firstStageNames = [
+            "Bulbasaur",
+            "Charmander",
+            "Squirtle",
+            "Oddish",
+            "Poliwag"
+        ];
+        return _firstStageNames[_index];
+    }
+
+    function secondStageNames(uint256 _index)
+        external
+        pure
+        returns (string memory)
+    {
+        string[5] memory _secondStageNames = [
+            "Ivysaur",
+            "Charmeleon",
+            "Wartortle",
+            "Gloom",
+            "Poliwhirl"
+        ];
+        return _secondStageNames[_index];
+    }
+
+    function thirdStageNames(uint256 _index)
+        external
+        pure
+        returns (string memory)
+    {
+        string[7] memory _thirdStageNames = [
+            "Venusaur",
+            "Charizard",
+            "Blastoise",
+            "Vileplume",
+            "Poliwrath",
+            "Bellossom",
+            "Politoed"
+        ];
+        return _thirdStageNames[_index];
+    }
+}
