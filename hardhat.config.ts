5a7bebe76cc6e206b2a5844d6c11609147a05790
Bohdan Pukhno <pukhnobg@gmail.com>
2022-09-18 16:02:45 +0300

    update: Added 0x to address

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
   networks: {
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`${process.env.GOERLI_PRIVATE_KEY}`]
+      accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`]
     }
   }
 };
