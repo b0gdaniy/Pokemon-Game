69f51ab668065939bf14ed0fac2812976501a643
bohdan <57547705+r0ugeEngine@users.noreply.github.com>
2022-09-30 11:31:50 +0300

    add: Added separate enums

---
+++ contracts/Enums/PokemonsNum.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+enum PokemonsNum {
+    Venusaur,
+    Charizard,
+    Blastoise,
+    Vileplume,
+    Poliwrath,
+    Bellosom,
+    Politoed
+}
